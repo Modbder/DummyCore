c84e1b596de84df385043e285a0ad7627d723549
Modbder <Modbder@gmail.com>
2015-09-14 14:24:44 +0300

    [2.0]Code cleanup, a helping var of MC's directory

--- DummyCore/ASM/DCLoadingPlugin.java
+++ DummyCore/ASM/DCLoadingPlugin.java
@@ -1,38 +1,44 @@
 package DummyCore.ASM;
 
+import java.io.File;
 import java.util.Map;
 
+import DummyCore.Core.Core;
+import cpw.mods.fml.relauncher.FMLInjectionData;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import static DummyCore.Core.CoreInitialiser.mcVersion;
 
+@MCVersion(value=mcVersion)
 public class DCLoadingPlugin implements IFMLLoadingPlugin{
 
+	public DCLoadingPlugin()
+	{
+		Core.mcDir = (File) FMLInjectionData.data()[6];
+	}
+	
 	@Override
 	public String[] getASMTransformerClass() {
-		// TODO Auto-generated method stub
 		return new String[]{DCASMManager.class.getName()};
 	}
 
 	@Override
 	public String getModContainerClass() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getSetupClass() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void injectData(Map<String, Object> data) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public String getAccessTransformerClass() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
