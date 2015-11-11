89b98acb7f255f535457bc7e03d9bd464fd7fccf
Modbder <Modbder@gmail.com>
2015-11-12 00:08:33 +0300

    [1.8]Removed the hash functions from Pair, will be a wrapper later

--- DummyCore/Utils/Pair.java
+++ DummyCore/Utils/Pair.java
@@ -1,6 +1,6 @@
 package DummyCore.Utils;
 
-public class Pair<T1, T2> {
+public class Pair<T1, T2>{
 	
 	public T1 obj1;
 	public T2 obj2;
@@ -20,5 +20,22 @@ public class Pair<T1, T2> {
 		obj1 = o1;
 		obj2 = o2;
 	}
-
+	
+	@Override
+	public int hashCode()
+	{
+		return toString().hashCode();
+	}
+	
+	@Override
+	public String toString()
+	{
+		return obj1.toString()+","+obj2.toString();
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		return obj != null && obj.toString().equals(this.toString());
+	}
 }
