fe24c0acdb87fc3c8696cc8750edd92f96a5be2a
Modbder <Modbder@gmail.com>
2015-11-12 00:00:47 +0300

    [1.8]Returned the old .obj model system

---
+++ DummyCore/Client/obj/GroupObject.java
@@ -0,0 +1,54 @@
+package DummyCore.Client.obj;
+
+import java.util.ArrayList;
+
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class GroupObject
+{
+    public String name;
+    public ArrayList<Face> faces = new ArrayList<Face>();
+    public int glDrawingMode;
+
+    public GroupObject()
+    {
+        this("");
+    }
+
+    public GroupObject(String name)
+    {
+        this(name, -1);
+    }
+
+    public GroupObject(String name, int glDrawingMode)
+    {
+        this.name = name;
+        this.glDrawingMode = glDrawingMode;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void render()
+    {
+        if (faces.size() > 0)
+        {
+            Tessellator tessellator = Tessellator.getInstance();
+            tessellator.getWorldRenderer().startDrawing(glDrawingMode);
+            render(tessellator);
+            tessellator.draw();
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void render(Tessellator tessellator)
+    {
+        if (faces.size() > 0)
+        {
+            for (Face face : faces)
+            {
+                face.addFaceForRender(tessellator);
+            }
+        }
+    }
+}
---
+++ DummyCore/Client/obj/ObjModelLoader.java
@@ -0,0 +1,29 @@
+package DummyCore.Client.obj;
+
+import DummyCore.Client.IModelCustom;
+import DummyCore.Client.IModelCustomLoader;
+import DummyCore.Client.ModelFormatException;
+import net.minecraft.util.ResourceLocation;
+
+public class ObjModelLoader implements IModelCustomLoader
+{
+
+    @Override
+    public String getType()
+    {
+        return "OBJ model";
+    }
+
+    private static final String[] types = { "obj" };
+    @Override
+    public String[] getSuffixes()
+    {
+        return types;
+    }
+
+    @Override
+    public IModelCustom loadInstance(ResourceLocation resource) throws ModelFormatException
+    {
+        return new WavefrontObject(resource);
+    }
+}
