7c58e364fb0fb986853a2da542a843d575c69e41
Modbder <Modbder@gmail.com>
2015-11-12 00:12:42 +0300

    [1.8]Custom step sound...

---
+++ DummyCore/Utils/CustomStepSound.java
@@ -0,0 +1,11 @@
+package DummyCore.Utils;
+
+import net.minecraft.block.Block.SoundType;
+
+public class CustomStepSound extends SoundType{
+
+	public CustomStepSound(String name, float volume, float frequency) {
+		super(name, volume, frequency);
+	}
+
+}
